bf18589ba11e146de07b39cbc75f2df9e610cbb8
Said Ouarrak <32547160+saidtechnology@users.noreply.github.com>
2024-02-01 18:21:53 +0000

    Update siteProps and image assets

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Said Ouarrak",
+  title: "Web Developer & Mobile App Developer",
+  email: "saidx5@gmail.com",
+  gitHub: "saidtechnology",
+  instagram: "saidtechnology",
+  linkedIn: "saidtechnology",
+  medium: "saidtechnology",
+  twitter: "saidtechnology",
+  youTube: "saidtechnology",
 };
 
 const primaryColor = "#4E567E";

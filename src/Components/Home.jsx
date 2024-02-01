febb56dd893173dcd53d00a2f5dfe4a5bb986571
Said Ouarrak <32547160+saidtechnology@users.noreply.github.com>
2024-02-01 21:36:34 +0000

    Mise à jour de la page d'accueil avec un style amélioré

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -13,7 +13,7 @@ import PropTypes from "prop-types";
  * Home background image
  *
  * Below is a sample image. Upload the image of your choice into the "images"
- * directory and import here for use. Then, set imageAltText to string that 
+ * directory and import here for use. Then, set imageAltText to string that
  * represents what you see in that image.
  *
  *
@@ -28,7 +28,9 @@ const Home = ({ name, title }) => {
   return (
     <section id="home" className="min-height">
       <img className="background" src={image} alt="" />
-      <div style={{ position: "absolute", top: "5rem", left: "2rem", width: "17rem" }}>
+      <div
+        style={{ color: "blue", position: "absolute", top: "5rem", left: "2rem", width: "27rem" }}
+      >
         <h1>{name}</h1>
         <h2>{title}</h2>
       </div>
